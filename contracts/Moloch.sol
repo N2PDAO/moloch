d43adfcc4aad5c7e029ceb2569ee62116197b784
mfahampshire <mfahampshire@gmail.com>
2019-04-28 16:45:47 +0200

    lint fix

--- contracts/Moloch.sol
+++ contracts/Moloch.sol
@@ -81,7 +81,7 @@ contract Moloch {
 
     mapping (address => Member) public members;
     mapping (address => address) public memberAddressByDelegateKey;
-    mapping(address => mapping(address => uint256)) public sharesDelegated; // mapping of delegator => delegatee => shares delegated
+    mapping (address => mapping(address => uint256)) public sharesDelegated; // mapping of delegator => delegatee => shares delegated
     Proposal[] public proposalQueue;
 
     /********
