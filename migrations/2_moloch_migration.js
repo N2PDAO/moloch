e0aa1e70ef0499a3f789da3adb35f1bd8fd23eec
p <paulseidler1@gmail.com>
2019-04-30 21:18:34 +0200

    gas limit

--- migrations/2_moloch_migration.js
+++ migrations/2_moloch_migration.js
@@ -23,7 +23,7 @@ module.exports = (deployer, network, accounts) => {
       config.PROPOSAL_DEPOSIT,
       config.DILUTION_BOUND,
       config.PROCESSING_REWARD,
-      { gas: 6000000 }
+      { gas: 6500000 }
     )
   })
 }
